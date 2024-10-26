8de2988e970fac5282003ad1b0a6ee3bfb46c400
Simon <simon.vereecke@live.com>
2024-10-26 07:30:08 +0200

    add homepage mobile image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import HamburgerIcon from './assets/icons/hamburger.svg';
 import LogoIcon from './assets/logo.svg';
+import Image from './assets/homepage/mobile/image-homepage-hero.jpg';
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <>
       <header className='flex items-center justify-between p-8'><img src={LogoIcon} /><img src={HamburgerIcon} /></header>
       <main>
-        <section>Hey, I'm Alex Spencer and I love building beautiful websites</section>
+        <section><img src={Image} className='px-8 pt-2' /> Hey, I'm Alex Spencer and I love building beautiful websites</section>
         <section>
           <h1>About me</h1>
           <p>I'm a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I'm based in London, UK, but I'm happ working remotely and have experience in remote teams. When I'm not coding, you'll find me outdoors. I love being out in nature whether that's going for a walk, run or cycling. I'd love you to check ou my work.</p>

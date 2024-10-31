19df54577c3d6f883575b4ba3773afa87726f8ce
Simon <simon.vereecke@live.com>
2024-10-31 10:47:57 +0100

    add profile pic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import "./App.css";
 import HamburgerIcon from "./assets/icons/hamburger.svg";
 import DownArrowsIcon from "./assets/icons/down-arrows.svg";
 import LogoIcon from "./assets/logo.svg";
-import Image from "./assets/homepage/mobile/image-homepage-hero.jpg";
+import HomepageImage from "./assets/homepage/mobile/image-homepage-hero.jpg";
+import ProfileImage from "./assets/homepage/mobile/image-homepage-profile.jpg";
 
 function App() {
   return (
@@ -12,26 +13,33 @@ function App() {
         <img src={HamburgerIcon} />
       </header>
       <main>
-        <section className="flex flex-col px-8 p-2">
-          <img src={Image} />
+        <section className="flex flex-col px-8 pt-2 pb-6">
+          <img src={HomepageImage} alt="homepage image" />
           <span className="font-ibarra font-bold text-4xl tracking-tight pt-6 pb-[1.85rem]">
             Hey, I'm Alex Spencer and I love building beautiful websites
           </span>
-          <button className="flex items-center h-12 max-w-[12.5rem] bg-red-600">
-            <img src={DownArrowsIcon} className="px-4 py-4 bg-blue-600" />
-            <p>ABOUT ME</p>
+          <button className="flex items-center h-12 max-w-[12.5rem] bg-dark-blue">
+            <img src={DownArrowsIcon} className="h-full p-4 bg-dark-blue" />
+            <p className="font-publicSans w-full text-white text-xs font-extralight tracking-[0.15rem]">
+              ABOUT ME
+            </p>
           </button>
         </section>
-        <section>
+        <img
+          src={ProfileImage}
+          alt="profile picture"
+          className="px-8 pt-[4.5rem] pb-8"
+        />
+        <section className="flex flex-col px-8 border-t-[0.25px] border-solid border-light-grey">
           <p>
             I'm a junior front-end developer looking for a new role in an
             exciting company. I focus on writing accessible HTML, using modern
             CSS practices and writing clean JavaScript. When writing JavaScript
             code, I mostly use React, but I can adapt to whatever tools are
-            required. I'm based in London, UK, but I'm happ working remotely and
-            have experience in remote teams. When I'm not coding, you'll find me
-            outdoors. I love being out in nature whether that's going for a
-            walk, run or cycling. I'd love you to check ou my work.
+            required. I'm based in London, UK, but I'm happy working remotely
+            and have experience in remote teams. When I'm not coding, you'll
+            find me outdoors. I love being out in nature whether that's going
+            for a walk, run or cycling. I'd love you to check ou my work.
           </p>
         </section>
         <section>Interested in doing a project together?</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,18 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'bright-red': 'hsl(var(--color-bright-red) / <alpha-value>)',
+        'dark-blue': 'hsl(var(--color-dark-blue) / <alpha-value>)',
+        'grayish-dark-blue': 'hsl(var(--color-grayish-dark-blue) / <alpha-value>)',
+        'light-grey': 'hsl(var(--color-light-grey) / <alpha-value>)',
+        'slightly-desaturated-cyan': 'hsl(var(--color-slightly-desaturated-cyan) / <alpha-value>)',
+        'very-light-grey': 'hsl(var(--color-very-light-grey) / <alpha-value>)',
+      },
       fontFamily: {
         "rubik": ['Rubik', 'sans-serif'],
-        "ibarra": ['Ibarra Real Nova', 'sans-serif']
+        "ibarra": ['Ibarra Real Nova', 'sans-serif'],
+        "publicSans": ['Public Sans', 'sans-serif']
       },
       fontSize: {
         '4xl': ['2.5rem', '2.65rem']

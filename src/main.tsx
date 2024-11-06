007184d36dba98404388b690e104412acc399a49
Simon <simon.vereecke@live.com>
2024-11-06 21:25:50 +0100

    add routing to portfolio index

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Portfolio } from './Portfolio.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/minimalist-portfolio/",
+    element: <App />,
+  },
+  {
+    path: "/minimalist-portfolio/portfolio",
+    element: <Portfolio />
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

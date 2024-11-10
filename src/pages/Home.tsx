76990e1ca33aef3f263d97725ca7a3af6fde4718
Simon <simon.vereecke@live.com>
2024-11-10 12:03:05 +0100

    remove unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "../components";
 import HomepageImage from "./../assets/homepage/mobile/image-homepage-hero.jpg";
 import ProfileImage from "./../assets/homepage/mobile/image-homepage-profile.jpg";

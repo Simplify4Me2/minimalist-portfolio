dbee7d74bf399dc539b1e13e9e6fdeb0c765ac1f
Simon <simon.vereecke@live.com>
2024-11-13 21:30:05 +0100

    refactor Home to descriptive components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { Button } from "../components";
 import HomepageMobileImage from "./../assets/homepage/mobile/image-homepage-hero.jpg";
 import HomepageTabletImage from "./../assets/homepage/tablet/image-homepage-hero.jpg";
@@ -7,30 +6,15 @@ import ProfileImage from "./../assets/homepage/mobile/image-homepage-profile.jpg
 import DownArrowsIcon from "./../assets/icons/down-arrows.svg";
 import { Footer } from "../Footer";
 import { Header } from "../Header";
+import { PropsWithChildren } from "react";
 
 export function Home() {
-  const navigate = useNavigate();
-
   return (
     <>
       <Header />
       <main className="px-8 lg:px-[5.25rem] lg:pt-0">
-        <section className="flex flex-col pt-2 pb-6 relative">
-          <img
-            src={HomepageMobileImage}
-            alt="homepage image"
-            className="md:hidden"
-          />
-          <img
-            src={HomepageTabletImage}
-            alt="homepage image"
-            className="hidden md:block lg:hidden"
-          />
-          <img
-            src={HomepageDesktopImage}
-            alt="homepage image"
-            className="hidden lg:block"
-          />
+        <section className="flex flex-col pt-2 pb-[5.5rem] relative">
+          <HomePageImage />
           <div className="md:absolute md:bg-very-light-grey md:bottom-6 md:w-3/4 lg:w-2/5 lg:h-[22.5rem] lg:flex lg:flex-col lg:justify-end">
             <h1 className="font-ibarra font-bold text-4xl tracking-tight pt-6 pb-[1.85rem] md:w-3/4 lg:text-[3.1rem] lg:leading-[3rem] lg:w-[24.5rem] lg:py-16">
               Hey, I&rsquo;m Alex Spencer and I love building beautiful websites
@@ -43,16 +27,8 @@ export function Home() {
             </button>
           </div>
         </section>
-        <img
-          src={ProfileImage}
-          alt="profile picture"
-          className="pt-[4.5rem] pb-8"
-        />
-        <section className="flex flex-col pb-[2.1rem] border-y-[0.25px] border-solid border-light-grey">
-          <h1 className="font-ibarra font-bold text-4xl tracking-tight pt-8 pb-[1.9rem]">
-            About Me
-          </h1>
-          <p className="font-publicSans leading-[1.875rem] text-left pb-1.5">
+        <Section imageSrc={ProfileImage} title="About Me">
+          <p className="font-publicSans leading-[1.875rem] text-left pt-[0.25rem] pb-1.5 min-h-[11.75rem]">
             I'm a junior front-end developer looking for a new role in an
             exciting company. I focus on writing accessible HTML, using modern
             CSS practices and writing clean JavaScript. When writing JavaScript
@@ -62,24 +38,70 @@ export function Home() {
             find me outdoors. I love being out in nature whether that's going
             for a walk, run or cycling. I'd love you to check out my work.
           </p>
-          <Button
-            onClick={() => navigate("portfolio")}
-            variant="outlined"
-            className="self-start my-4"
-          >
+          <Button variant="outlined" className="mt-4 self-start">
             GO TO PORTFOLIO
           </Button>
-        </section>
-        <section className="flex flex-col text-center pt-[7.2rem] pb-20">
-          <span className="font-ibarra font-bold text-4xl tracking-tight">
-            Interested in doing a project together?
-          </span>
-          <Button variant="outlined" className="mt-10 self-center">
-            CONTACT ME
-          </Button>
-        </section>
+        </Section>
+        <ContactMeSection />
       </main>
       <Footer />
     </>
+  );
+}
+
+type SectionProps = {
+  title: string;
+  imageSrc: string;
+};
+
+function Section({
+  children,
+  imageSrc,
+  title,
+}: PropsWithChildren<SectionProps>) {
+  return (
+    <section className="flex flex-col pt-2 pb-[3.3rem] border-b-[0.25px] border-solid border-light-grey">
+      <img src={imageSrc} />
+      <hr className="my-8 border-y-[0.25px] border-solid border-light-grey" />
+      <h1 className="font-ibarra font-bold text-4xl tracking-tight pb-6">
+        {title}
+      </h1>
+      {children}
+    </section>
+  );
+}
+
+function HomePageImage() {
+  return (
+    <>
+      <img
+        src={HomepageMobileImage}
+        alt="homepage image"
+        className="md:hidden"
+      />
+      <img
+        src={HomepageTabletImage}
+        alt="homepage image"
+        className="hidden md:block lg:hidden"
+      />
+      <img
+        src={HomepageDesktopImage}
+        alt="homepage image"
+        className="hidden lg:block"
+      />
+    </>
+  );
+}
+
+function ContactMeSection() {
+  return (
+    <section className="flex flex-col text-center pt-[7.2rem] pb-20">
+      <span className="font-ibarra font-bold text-4xl tracking-tight">
+        Interested in doing a project together?
+      </span>
+      <Button variant="outlined" className="mt-10 self-center">
+        CONTACT ME
+      </Button>
+    </section>
   );
 }

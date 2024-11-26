c68e93ec0adcdbb1abcbfa8be0fe974ea37a24c4
Simon <simon.vereecke@live.com>
2024-11-26 20:19:18 +0100

    update contact me section for tablet design

--- src/pages/home/ContactMeSection.tsx
+++ src/pages/home/ContactMeSection.tsx
@@ -6,12 +6,12 @@ type ContactMeSectionProps = {
 
 export function ContactMeSection({ className } : ContactMeSectionProps) {
     return (
-      <section className={`flex flex-col lg:flex-row lg:justify-between lg:items-center lg:my-16 text-center ${className}`}>
-      <span className="font-ibarra font-bold text-4xl tracking-tight lg:w-[20.5rem] lg:text-left">
+      <section className={`flex flex-col md:flex-row md:justify-between md:items-center md:mt-2 md:mb-4 text-center ${className}`}>
+      <span className="font-ibarra font-bold text-4xl tracking-tight md:w-[20.5rem] md:text-left md:mr-6 lg:mr-0">
         Interested in doing a project together?
       </span>
-      <hr className="hidden lg:block w-[35rem] mt-8 mb-6 border-y-[0.25px] border-solid border-grayish-dark-blue/15" />
-      <Button variant="outlined" className="mt-10 lg:mt-0 self-center">
+      <hr className="hidden md:block md:w-28 lg:w-[35rem] mt-8 mb-6 border-y-[0.25px] border-solid border-grayish-dark-blue/15" />
+      <Button variant="outlined" className="mt-10 md:mt-0 self-center text-nowrap">
         CONTACT ME
       </Button>
     </section>

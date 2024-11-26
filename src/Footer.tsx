02b054c66a5226a6730ef649d71987f5fa9604dc
Simon <simon.vereecke@live.com>
2024-11-26 20:55:17 +0100

    adjust footer styling for tablet

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -6,11 +6,11 @@ import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
-    <footer className="w-full bg-grayish-dark-blue h-fit text-white py-[3.5rem] lg:py-6 lg:px-9 flex justify-center">
-      <div className="max-w-screen-xl flex flex-col gap-9 lg:flex-row justify-between items-center">
+    <footer className="w-full bg-grayish-dark-blue h-fit text-white py-[3.5rem] md:py-6 md:px-9 flex justify-center">
+      <div className="max-w-screen-xl flex flex-col gap-9 md:flex-row justify-between items-center">
         <img src={LogoIcon} className="invert" />
-        <nav className="lg:mr-[35rem] lg:pl-2">
-          <ul className="flex flex-col lg:flex-row justify-between text-center font-publicSans text-xs font-extralight tracking-[0.15rem] h-28 lg:h-fit lg:gap-10">
+        <nav className="md:mr-32 lg:mr-[35rem] md:pl-2">
+          <ul className="flex flex-col md:flex-row justify-between text-center font-publicSans text-xs font-extralight tracking-[0.15rem] h-28 md:h-fit md:gap-10">
             <li>
               <Link to="/">HOME</Link>
             </li>

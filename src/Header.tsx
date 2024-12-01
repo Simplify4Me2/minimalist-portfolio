01f93a111eb06f08149ca20dc1b53893f2847760
Simon <simon.vereecke@live.com>
2024-12-01 20:27:23 +0100

    add Active Link Highlight

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import LogoIcon from "./assets/logo.svg";
 import HamburgerIcon from "./assets/icons/hamburger.svg";
 import CloseIcon from "./assets/icons/close.svg";
 
 export function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
   return (
     <header className="max-w-screen-md lg:max-w-screen-xl w-full flex items-center justify-between p-8 md:pt-16 md:pb-12 md:px-10 lg:px-[5.25rem] relative">
@@ -33,13 +34,38 @@ export function Header() {
       <nav className="hidden md:block">
         <ul className="md:flex gap-[42px] font-publicSans text-xs tracking-[0.125rem]">
           <li>
-            <Link to="/">HOME</Link>
+            <Link
+              to="/"
+              className={`${
+                location.pathname === "/" ? "text-blue-500 font-bold" : ""
+              }`}
+            >
+              HOME
+            </Link>
           </li>
           <li>
-            <Link to="/portfolio">PORTFOLIO</Link>
+            <Link
+              to="/portfolio"
+              className={`${
+                location.pathname === "/portfolio"
+                  ? "text-blue-500 font-bold"
+                  : ""
+              }`}
+            >
+              PORTFOLIO
+            </Link>
           </li>
           <li>
-            <Link to="/contact">CONTACT ME</Link>
+            <Link
+              to="/contact"
+              className={`${
+                location.pathname === "/contact"
+                  ? "text-blue-500 font-bold"
+                  : ""
+              }`}
+            >
+              CONTACT ME
+            </Link>
           </li>
         </ul>
       </nav>

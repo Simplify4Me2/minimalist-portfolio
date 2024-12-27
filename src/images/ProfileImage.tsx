16da3b826f0551f553110218b88197feeea6abda
Simon <simon.vereecke@live.com>
2024-12-27 21:49:21 +0100

    add TODO

--- src/images/ProfileImage.tsx
+++ src/images/ProfileImage.tsx
@@ -23,3 +23,20 @@ export function ProfileImage() {
       </>
     );
   }
+
+  
+// TODO: Refactor the ProfileImage component to use the srcSet attribute
+// import ProfileMobileImage from "../assets/homepage/mobile/image-homepage-profile.jpg";
+// import ProfileTabletImage from "../assets/homepage/tablet/image-homepage-profile.jpg";
+// import ProfileDesktopImage from "../assets/homepage/desktop/image-homepage-profile.jpg";
+
+// export function ProfileImage() {
+//     return (
+//       <img
+//         src={ProfileMobileImage}
+//         srcSet={`${ProfileMobileImage} 320w, ${ProfileTabletImage} 768w, ${ProfileDesktopImage} 1280w`}
+//         sizes="(max-width: 640px) 100%, (max-width: 768px) 100%, 100%"
+//         alt="homepage image"
+//       />
+//     );
+//   }

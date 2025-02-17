f82babfa5d1193c8f795ae4fa5fb936ad68b02d4
Simon <simon.vereecke@live.com>
2025-02-17 22:25:17 +0100

    use base image

--- src/images/ManageHeroImage.tsx
+++ src/images/ManageHeroImage.tsx
@@ -1,5 +1,17 @@
-import MobileManageHeroImage from "../assets/detail/mobile/image-manage-hero.jpg";
+import { Image } from "./Image";
+import MobileImage from "../assets/detail/mobile/image-manage-hero.jpg";
+import TabletImage from "../assets/detail/tablet/image-manage-hero.jpg";
+import DesktopImage from "../assets/detail/desktop/image-manage-hero.jpg";
 
 export function ManageHeroImage() {
-    return <img src={MobileManageHeroImage} /> 
+  return (
+    <Image
+      mobileImage={MobileImage}
+      tabletImage={TabletImage}
+      desktopImage={DesktopImage}
+      alt="Preview picture of the Manage project"
+    />
+  );
 }
+
+

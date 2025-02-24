e56f93eac800481aa7677bd70babc3d68e6f77f2
Simon <simon.vereecke@live.com>
2025-02-24 23:17:31 +0100

    rework portfolio navigation

--- src/components/PortfolioNavigation.tsx
+++ src/components/PortfolioNavigation.tsx
@@ -16,15 +16,15 @@ export function PortfolioNavigation({
   nextProjectLink,
 }: PortfolioNavigationProps) {
   return (
-    <nav className="max-w-screen-md lg:max-w-screen-xl flex w-full px-6 lg:px-20 mt-[4.25rem] lg:mt-12 ">
+    <nav className="max-w-screen-md lg:max-w-screen-xl flex w-full px-6 lg:px-[5.25rem] mt-[4.25rem] lg:mt-16 ">
       <Link
         to={previousProjectLink}
-        className="flex-1 border-black border-grayish-dark-blue/[.15] border-y-[1px]"
+        className="flex-1 lg:flex lg:justify-start lg:items-center border-black border-grayish-dark-blue/[.15] border-y-[1px]"
       >
         <img src={ArrowLeft} className="pt-[1.4rem] pb-4" />
-        <div>
+        <div className="lg:flex lg:flex-col lg:p-8 lg:pb-10">
           <p className="font-serif text-3xl">{previousProject}</p>
-          <p className="font-sans tracking-wide pt-1 pb-6 opacity-50">
+          <p className="font-sans tracking-wide pt-1 pb-6 lg:py-0 opacity-50">
             Previous Project
           </p>
         </div>
@@ -32,12 +32,12 @@ export function PortfolioNavigation({
       <span className="border-l-[1px] border-grayish-dark-blue/[.15]"></span>
       <Link
         to={nextProjectLink}
-        className="flex-1 flex flex-col text-right border-grayish-dark-blue/[.15] border-solid border-y-[1px]"
+        className="flex-1 flex flex-col items-end lg:justify-start lg:flex-row-reverse lg:items-center text-right border-grayish-dark-blue/[.15] border-solid border-y-[1px]"
       >
-        <img src={ArrowRight} className="self-end pt-[1.4rem] pb-4" />
-        <div>
+        <img src={ArrowRight} className="pt-[1.4rem] pb-4" />
+        <div className="lg:flex lg:flex-col lg:p-8 lg:pb-10">
           <p className="font-serif text-3xl">{nextProject}</p>
-          <p className="font-sans tracking-wide pt-1 pb-6 opacity-50">
+          <p className="font-sans tracking-wide pt-1 pb-6 lg:py-0 opacity-50">
             Next Project
           </p>
         </div>

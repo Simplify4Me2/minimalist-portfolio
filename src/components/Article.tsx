ffb4827d1fc5d70b2a2d63642a21d8321e7dbc43
Simon <simon.vereecke@live.com>
2025-03-06 15:47:17 +0100

    should I add tests to see the impact of these reused components?

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -8,7 +8,8 @@ export function Article({
 }: PropsWithChildren<{ title: string; className?: string }>) {
   return (
     <article className={`flex ${className}`}>
-      <div className="flex flex-col flex-1 justify-start lg:pb-7 pt-6 md:pt-8 lg:pt-12 border-y-[0.25px] border-solid border-grayish-dark-blue/15">
+      {/* <div className="flex flex-col flex-1 justify-start lg:pb-7 pt-6 md:pt-8 lg:pt-12 border-y-[0.25px] border-solid border-grayish-dark-blue/15"> */}
+      <div className="flex flex-col md:grid md:grid-cols-2 lg:flex lg:flex-col flex-1 justify-start md:pb-2 lg:pb-7 pt-6 md:pt-8 lg:pt-12 border-y-[0.25px] border-solid border-grayish-dark-blue/15">
         <ArticleHeader className="pb-1">{title}</ArticleHeader>
         {children}
       </div>

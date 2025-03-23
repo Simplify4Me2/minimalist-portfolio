1bfbcedaaee46db0e868d15484215d5bd590bf47
Simon <simon.vereecke@live.com>
2025-03-23 10:59:14 +0100

    update manage section

--- src/pages/portfolio/Overview.tsx
+++ src/pages/portfolio/Overview.tsx
@@ -14,10 +14,10 @@ function Overview() {
 
   return (
     <main className="max-w-screen-md lg:max-w-screen-xl px-8 md:px-10 lg:px-[5.25rem] lg:pt-12">
-      <Section className="my-2 md:my-8 lg:mt-0 gap-8">
+      <Section className="my-2 md:mt-[2.8rem] md:mb-8 lg:mt-0 gap-8">
         <ManageImage />
-        <Article title="Manage" className="pt-0 lg:pt-4 lg:px-4">
-          <TextBlock className="pb-6 pr-2 md:pb-10">
+        <Article title="Manage" className="lg:px-24">
+          <TextBlock className="pr-2 md:pr-0 md:pb-[2.15rem] lg:pb-6">
             This project required me to build a fully responsive landing page to
             the designs provided. I used HTML5, along with CSS Grid and
             JavaScript for the areas that required interactivity, such as the
@@ -30,7 +30,7 @@ function Overview() {
       <Section className="mt-[4.5rem] lg:mt-[4.85rem] gap-8 md:flex-row-reverse">
         <BookmarkImage />
         <Article title="Bookmark" className="lg:items-center">
-          <TextBlock className="pb-6 pr-2">
+          <TextBlock className="pr-2">
             This project required me to build a fully responsive landing page to
             the designs provided. I used HTML5, along with CSS Grid and
             JavaScript for the areas that required interactivity, such as the
@@ -80,7 +80,7 @@ type TextBlockProps = {
 function TextBlock({ children, className }: PropsWithChildren<TextBlockProps>) {
   return (
     <p
-      className={`font-publicSans tracking-tight lg:tracking-normal text-[0.95rem] lg:text-base opacity-80 leading-[1.875rem] lg:leading-[1.875rem] text-left pt-5 lg:pt-6 pb-6 min-h-[11.75rem] ${className}`}
+      className={`font-publicSans tracking-tight lg:tracking-normal text-[0.95rem] lg:text-base opacity-80 leading-[1.875rem] lg:leading-[1.875rem] text-left pt-5 md:pt-[1.65rem] lg:pt-6 pb-6 min-h-[11.75rem] ${className}`}
     >
       {children}
     </p>
@@ -94,7 +94,7 @@ function Article({
 }: PropsWithChildren<{ title: string; className?: string }>) {
   return (
     <article className={`flex ${className}`}>
-      <div className="flex flex-col flex-1 justify-start lg:pb-7 pt-6 md:pl-9 lg:pl-0 md:pt-12 border-y-[0.25px] border-solid border-grayish-dark-blue/15">
+      <div className="flex flex-col flex-1 justify-start lg:pb-7 md:ml-9 lg:ml-0 pt-6 md:pt-8 lg:pt-[6.25rem] md:pb-[1.6rem] border-y-[0.25px] border-solid border-grayish-dark-blue/15">
         <ArticleHeader className="pb-1">{title}</ArticleHeader>
         {children}
       </div>

10afc526a9c523f7cc5588328ed8aee70f9fb90b
Simon <simon.vereecke@live.com>
2025-03-27 22:52:21 +0100

    update button arrows stroke

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import DownArrowsIcon from "./../assets/icons/down-arrows.svg";
+import WhiteDownArrowsIcon from "./../assets/icons/white-down-arrows.svg";
 
 interface IButtonProps {
   variant: "contained" | "contained-with-down-arrow" | "outlined";
@@ -16,7 +17,8 @@ export function Button({
   if (variant === "contained-with-down-arrow")
     return (
       <button className="flex items-center h-12 w-[12.5rem] bg-dark-blue hover:bg-slightly-desaturated-cyan group">
-        <img src={DownArrowsIcon} className="h-full p-4 group-hover:filter group-hover:invert" />
+        <img src={DownArrowsIcon} className="h-full p-4 group-hover:filter group-hover:hidden" />
+        <img src={WhiteDownArrowsIcon} className="h-full p-4 hidden group-hover:filter group-hover:inline-block" />
         <p className="font-publicSans w-full text-white text-xs font-extralight tracking-[0.15rem]">
           {children}
         </p>

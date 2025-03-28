d37e70421fed5e72446c5afbccf5eea38148c357
Simon <simon.vereecke@live.com>
2025-03-28 21:35:29 +0100

    scroll about me section into view

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,9 +3,11 @@ import { PropsWithChildren } from "react";
 export function Section({
   children,
   className = '',
-}: PropsWithChildren<{ className?: string }>) {
+  ref
+}: PropsWithChildren<{ className?: string, ref?: React.Ref<HTMLDivElement> }>) {
   return (
     <section
+      ref={ref}
       className={`flex flex-col md:flex-row lg:justify-between ${className}`}
     >
       {children}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,9 +10,16 @@ import {
   ProjectLink,
   Section,
 } from "../../components";
+import { useRef } from "react";
 
 function Home() {
   useScrollToTop();
+
+  const aboutMeRef = useRef<HTMLDivElement>(null);
+
+  const scrollToAboutMe = () => {
+    aboutMeRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <>
@@ -23,7 +30,7 @@ function Home() {
             <ArticleHeader className="grow pt-6 md:pt-14 pb-[1.85rem] max-w-md md:w-3/4 lg:text-[3.1rem] lg:leading-[3.1rem] lg:w-[24.5rem] lg:pt-16 lg:pb-14 lg:tracking-normal">
               Hey, I&rsquo;m Alex Spencer and I love building beautiful websites
             </ArticleHeader>
-            <Button onClick={() => {}} className="group">
+            <Button onClick={scrollToAboutMe} className="group">
               <img src={DownArrowsIcon} className="h-full p-4 group-hover:filter group-hover:hidden" />
               <img src={WhiteDownArrowsIcon} className="h-full p-4 hidden group-hover:filter group-hover:inline-block" />
               <p className="font-publicSans w-full text-white text-xs font-extralight tracking-[0.15rem]">
@@ -33,7 +40,7 @@ function Home() {
           </div>
         </section>
 
-        <Section className="pt-2 lg:pt-0 lg:mt-16 gap-8">
+        <Section ref={aboutMeRef} className="pt-2 lg:pt-0 lg:mt-16 gap-8">
           <ProfileImage />
           <article className="flex lg:px-24">
             <div className="flex flex-col flex-1 justify-start pb-[1.65rem] lg:pb-5 pt-[1.9rem] md:ml-9 lg:ml-0 md:pt-12 border-y-[0.25px] border-solid border-grayish-dark-blue/15">
